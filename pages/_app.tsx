a3b9d565d9fb9991da17d01d00c035e98f78d841
tomide <ayotomiiide@gmail.com>
2022-01-19 23:22:22 +0000

    ft: setup apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.thegraph.com/subgraphs/name/hop-protocol/hop-mainnet",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp
